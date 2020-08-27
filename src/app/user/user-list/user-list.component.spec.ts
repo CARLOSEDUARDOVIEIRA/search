11afe9b66f87b110787f10abc6cfdc54376c06db
Carlos Eduardo Vieira <dullvieira@gmail.com>
2020-08-27 14:16:16 -0300

    feature: user list criacao testes

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { UserListMock } from './../../core/mocks/users.mock';
+import { By } from '@angular/platform-browser';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
@@ -17,9 +19,24 @@ describe('UserListComponent', () => {
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    component.userList = UserListMock;
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Deve criar', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Deve exibir listar os cards de todos os usuários recebidos', () => {
+    const allCardsHtml = fixture.debugElement.queryAll(By.css('.user-card'));
+    expect(allCardsHtml.length).toEqual(component.userList.length);
+  });
+
+  it('Ao clicar para ver detalhes do usuário Deve emitir um evento chamando detalhes do usuário', () => {
+    spyOn(component['callUser'], 'next').and.returnValue(null);
+    const userCardHtml = fixture.debugElement.query(By.css('.users-card > div')).nativeElement;
+    userCardHtml.click();
+    expect(component['callUser'].next).toHaveBeenCalled();
   });
 });
