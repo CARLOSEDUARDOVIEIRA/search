b01faa3f7560c43d4e9c42315f13f1e713c9ba08
Carlos Eduardo Vieira <dullvieira@gmail.com>
2020-08-27 15:10:56 -0300

    feature: header component criacao testes

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -19,7 +20,22 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Deve ser criado', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Deve exibir a logo corretamente', () => {
+    const logoHtml = fixture.debugElement.query(By.css('.logo > img')).nativeElement;
+    expect(logoHtml.src).toContain('/assets/logo.png');
+  });
+
+  it('Deve exibir o título corretamente', () => {
+    const titleHtml = fixture.debugElement.query(By.css('.title__begin')).nativeElement;
+    expect(titleHtml.textContent).toEqual('BuscaHub');
+  });
+
+  it('Deve exibir o subtitulo corretamente', () => {
+    const subTitleHtml = fixture.debugElement.query(By.css('.subtitle')).nativeElement;
+    expect(subTitleHtml.textContent).toEqual('Uma nova experiência de encontrar usuários Github');
   });
 });
